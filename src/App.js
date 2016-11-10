0c6c3965838f410c4d598bb42d70b9ffc9cf7729
joselevelsup <joselevelsup@gmail.com>
2016-11-10 16:30:54 -0500

    Set up app for heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import ReactSwipe from 'react-swipe';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Carousel extends React.Component {
+    render() {
+        return (
+            <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
+                <div>PANE 1</div>
+                <div>PANE 2</div>
+                <div>PANE 3</div>
+            </ReactSwipe>
+        );
+    }
 }
 
-export default App;
+ReactDOM.render(
+    <Carousel />, 
+    document.getElementById('app')
+);
+
+
+
+export default Carousel;
