be16536474dfb8c97d4cf82dec8a43eedd104eb5
joselevelsup <joselevelsup@gmail.com>
2016-11-10 16:44:29 -0500

    Testing first swipe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
 import ReactSwipe from 'react-swipe';
 
-class Carousel extends React.Component {
-    render() {
-        return (
+var App = React.createClass({
+  render: function(){
+     return (
             <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
                 <div>PANE 1</div>
                 <div>PANE 2</div>
                 <div>PANE 3</div>
             </ReactSwipe>
         );
-    }
-}
+ 
+
+
+  }
+
+
+
+})
+   
 
 ReactDOM.render(
-    <Carousel />, 
+    <App />, 
     document.getElementById('app')
 );
 
 
 
-export default Carousel;
+export default App;
