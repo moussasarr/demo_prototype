6eb1e0a81238016345ca9fa4f10151ce836aafdb
joselevelsup <joselevelsup@gmail.com>
2016-11-10 22:54:45 -0500

    making sure slide functionality is back

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Carousel extends React.Component {
     super();
     
     this.state= {
-        slides: [<img src='images/image1.jpg' role="presentation"/>, <img src='images/image2.jpg' role="presentation"/>, <img src='images/image3.jpg' role="presentation"/>, <img src='images/image4.jpg' role="presentation"/>, <img src='images/image5.jpg' role="presentation"/>, <img src='images/image6.jpg' role="presentation" />, <img src='images/image7.jpg' role="presentation"/>, <img src='images/image8.jpg' role="presentation"/>, <img src='images/image9.jpg' role="presentation"/>, <img src='images/image10.jpg' role="presentation"/>, <img src='images/image11.jpg' role="presentation"/>, <img src='images/image12.jpg' role="presentation"/>, <img src='images/image13.jpg' role="presentation"/>, <img src='images/image14.jpg' role="presentation"/>, <img src='images/image15.jpg' role="presentation"/>]};
+        slides: [<img src='images/image1.jpg' role="presentation" />, <img src='images/image2.jpg' role="presentation"/>, <img src='images/image3.jpg' role="presentation"/>, <img src='images/image4.jpg' role="presentation"/>, <img src='images/image5.jpg' role="presentation"/>, <img src='images/image6.jpg' role="presentation" />, <img src='images/image7.jpg' role="presentation"/>, <img src='images/image8.jpg' role="presentation"/>, <img src='images/image9.jpg' role="presentation"/>, <img src='images/image10.jpg' role="presentation"/>, <img src='images/image11.jpg' role="presentation"/>, <img src='images/image12.jpg' role="presentation"/>, <img src='images/image13.jpg' role="presentation"/>, <img src='images/image14.jpg' role="presentation"/>, <img src='images/image15.jpg' role="presentation"/>]};
   }    
   
   componentDidUpdate(prevProps, prevState) {
@@ -36,7 +36,7 @@ class Carousel extends React.Component {
 
   render() {
     return (
-      <div id="myreactswipe">
+      <div>
         <ReactSwipe ref="reactSwipe" className="carousel">
           {this.state.slides.map(slide => {
             return (
@@ -56,6 +56,7 @@ class Carousel extends React.Component {
     );
   }
 }
+
 
 ReactDOM.render(
   <Carousel />,
