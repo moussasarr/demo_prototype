341a20709f88ef8f0b511b7ced972ea2b788fcd5
Moussa Sarr <moussasarr490@gmail.com>
2016-11-11 03:09:46 -0500

    Removed responsiveness

--- src/index.js
+++ src/index.js
@@ -14,21 +14,21 @@ var Home = React.createClass({
 
     <div className="homeView">
       <div className="row header">
-       <div className= "col-lg-3">
+       <div className= "col-xs-3">
         <h2> Header 1</h2>
        </div>
 
-       <div className= "col-lg-3">
+       <div className= "col-xs-3">
         <h2> Header 2</h2>
        </div>
 
 
-      <div className= "col-lg-3">
+      <div className= "col-xs-3">
         <h2> Header 3</h2>
        </div>
    
 
-       <div className= "col-lg-3">
+       <div className= "col-xs-3">
         <h2> Header 4</h2>
        </div>
     </div>
@@ -38,21 +38,21 @@ var Home = React.createClass({
 
 
      <div className="row intro">
-       <div className="col-lg-3">
+       <div className="col-xs-3">
         <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
        </div>
 
-       <div className="col-lg-3">
+       <div className="col-xs-3">
         <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
        </div>
 
 
-      <div className="col-lg-3">
+      <div className="col-xs-3">
         <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
        </div>
    
 
-       <div className="col-lg-3">
+       <div className="col-xs-3">
         <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
        </div>
      </div>
@@ -64,21 +64,21 @@ var Home = React.createClass({
 
 
      <div className="row pic">
-       <div className="col-lg-3">
+       <div className="col-xs-3">
          <img src='images/image1'  style={{width: '25vw', height: '25vh' }}/>
        </div>
 
-       <div className="col-lg-2">
+       <div className="col-xs-2">
         <img src='images/image3'  style={{width: '25vw', height: '25vh' }}/>
        </div>
 
 
-      <div className="col-lg-3">
+      <div className="col-xs-3">
         <img src='images/image3' style={{width: '25vw', height: '25vh' }}/> 
       </div>
    
 
-       <div className="col-lg-3">
+       <div className="col-xs-3">
         <img src='images/image4'  style={{width: '25vw', height: '25vh' }}/> 
        </div>
 
