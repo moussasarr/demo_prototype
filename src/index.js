f3e6e2e533695a82af99e7bcf5599c0230bb49d1
Moussa Sarr <moussasarr490@gmail.com>
2016-11-11 03:00:02 -0500

    Getting homeviewq in shape

--- src/index.js
+++ src/index.js
@@ -6,53 +6,131 @@ import ReactSwipe from 'react-swipe'
 
 
 
+var Home = React.createClass({
+   render: function(){
 
-class Carousel extends React.Component {
-  constructor() {
-    super();
-    
-    this.state = {
-        slides: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg']
-    };
-  }    
-  
-  componentDidUpdate(prevProps, prevState) {
-    // due to buggy iframe behavior
-    window.dispatchEvent(new Event('resize'));
-  }
-  
-  next() {
-    this.refs.reactSwipe.next();
-  }
+    return(
 
-  prev() {
-    this.refs.reactSwipe.prev();
-  }
-  
-  addSlide() {
-    const slides = this.state.slides;
-    slides.push(`pane ${slides.length + 1}`);
-    this.setState({ slides });
-  }
 
-  render() {
-    return (
-      <div>
-         <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
-          {this.state.slides.map(slide => {
-            return (
-              <div key={slide} className="item">
-                <img src={slide} alt="Person" style={{width: '80vw', height: '80vh' }}/>
-              </div>
-            );
-          })}
-        </ReactSwipe>
+    <div className="homeView">
+      <div className="row header">
+       <div className= "col-lg-3">
+        <h2> Header 1</h2>
+       </div>
+
+       <div className= "col-lg-3">
+        <h2> Header 2</h2>
+       </div>
+
+
+      <div className= "col-lg-3">
+        <h2> Header 3</h2>
+       </div>
+   
+
+       <div className= "col-lg-3">
+        <h2> Header 4</h2>
+       </div>
+    </div>
+
+
+
+
+
+     <div className="row intro">
+       <div className="col-lg-3">
+        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
+       </div>
+
+       <div className="col-lg-3">
+        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
+       </div>
+
+
+      <div className="col-lg-3">
+        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
+       </div>
+   
+
+       <div className="col-lg-3">
+        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
+       </div>
+     </div>
+
+
+
+
+
+
+
+     <div className="row pic">
+       <div className="col-lg-3">
+         <img src='images/image1'  style={{width: '25vw', height: '25vh' }}/>
+       </div>
+
+       <div className="col-lg-2">
+        <img src='images/image3'  style={{width: '25vw', height: '25vh' }}/>
+       </div>
+
+
+      <div className="col-lg-3">
+        <img src='images/image3' style={{width: '25vw', height: '25vh' }}/> 
       </div>
-    );
-  }
-}
+   
+
+       <div className="col-lg-3">
+        <img src='images/image4'  style={{width: '25vw', height: '25vh' }}/> 
+       </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+     
+      );
+     }
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(
-  <Carousel />,
+  <Home />,
   document.getElementById('root')
 );
