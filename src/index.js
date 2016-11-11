d5571f54e8c86970a20a9f359b092324092727b0
Moussa Sarr <moussasarr490@gmail.com>
2016-11-11 05:25:33 -0500

    Some Demo done

--- src/index.js
+++ src/index.js
@@ -1,160 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ReactSwipe from 'react-swipe'
+import Home from './modules/Home';
+import Carousel from './modules/Carousel';
+import { Router, Route, hashHistory } from 'react-router';
 
 
 
 
-var Home = React.createClass({
-   render: function(){
 
-    return(
-
-
-    <div className="homeView">
-      <div className="row header">
-       <div className= "col-xs-3">
-       <br /><br />
-        <h2> Header 1</h2>
-       </div>
-
-       <div className= "col-xs-3">
-       <br /><br />
-        <h2> Header 2</h2>
-       </div>
-
-
-      <div className= "col-xs-3">
-      <br /><br />
-        <h2> Header 3</h2>
-       </div>
-   
-
-       <div className= "col-xs-3">
-       <br /><br />
-        <h2> Header 4</h2>
-       </div>
-    </div>
-
-
-
-
-
-     <div className="row intro">
-       <div className="col-xs-3">
-        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
-       </div>
-
-       <div className="col-xs-3">
-        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
-       </div>
-
-
-      <div className="col-xs-3">
-        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
-       </div>
-   
-
-       <div className="col-xs-3">
-        <p> Lorem ipsum dolor sit amet, utamur persius definiebas qui cu. Te pertinax inciderint eum, et consul animal sapientem ius, his debitis abhorreant efficiantur an. Et oportere indoctum quo, duo ad modo inciderint. Cu odio aeque oportere usu, wisi viris molestie te vel, ut pro labitur ponderum definiebas. Eu pro deseruisse constituam conclusionemque, vis splendide intellegat ullamcorper in. Stet zril nonumy cu vel, eum esse malis necessitatibus ne, ad pri modus intellegat.</p>
-       </div>
-     </div>
-
-
-
-
-
-
-
-     <div className="row pic">
-       <div className="col-xs-3">
-         <img src='images/image1'  style={{width: '25vw', height: '25vh' }}/>
-       </div>
-
-       <div className="col-xs-3">
-        <img src='images/image2'  style={{width: '25vw', height: '25vh' }}/>
-       </div>
-
-
-      <div className="col-xs-3">
-        <img src='images/image3' style={{width: '25vw', height: '25vh' }}/> 
-      </div>
-   
-
-       <div className="col-xs-3">
-        <img src='images/image4'  style={{width: '25vw', height: '25vh' }}/> 
-       </div>
-
-    </div>
-
-
-    <div className="row ">
-       <div className="col-xs-3" style={{display: 'none'}}>
-         
-       </div>
-
-       <div className="col-xs-3" style={{width: '25vw', height: '25vh', background: 'white', border:'white' }}>
-       
-       </div>
-
-
-      <div className="col-xs-3">
-        <img src='images/image3' style={{width: '25vw', height: '25vh' }}/> 
-      </div>
-   
-
-       <div className="col-xs-3" style={{display: 'none'}}>
-         
-       </div>
-
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
-     
-      );
-     }
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="/swipe" component={Carousel}/>
+  </Router>
+), document.getElementById('root'))
---
+++ src/modules/Carousel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactSwipe from 'react-swipe'
+
+class Carousel extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+        slides: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg']
+    };
+  }    
+  
+  componentDidUpdate(prevProps, prevState) {
+    // due to buggy iframe behavior
+    window.dispatchEvent(new Event('resize'));
+  }
+  
+  next() {
+    this.refs.reactSwipe.next();
+  }
+
+  prev() {
+    this.refs.reactSwipe.prev();
+  }
+  
+  addSlide() {
+    const slides = this.state.slides;
+    slides.push(`pane ${slides.length + 1}`);
+    this.setState({ slides });
+  }
+
+  render() {
+    return (
+      <div>
+         <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
+          {this.state.slides.map(slide => {
+            return (
+              <div key={slide} className="item">
+                <img src={slide} alt="Person" style={{width: '80vw', height: '80vh' }}/>
+              </div>
+            );
+          })}
+        </ReactSwipe>
+      </div>
+    );
+  }
+}
+
+
+export default Carousel;
